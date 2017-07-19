570207892b56b1dc5cd88bc242cd959a2faad24b
Michael Nisi <michael.nisi@gmail.com>
2017-07-19 10:47:04 +0200

    Create subscription tables

--- FeedKit/user.sql
+++ FeedKit/user.sql
@@ -11,6 +11,35 @@ pragma user_version = 1;
 
 begin immediate transaction;
 
+-- Records
+
+create table if not exists record(
+  record_name text primary key,
+  change_tag text
+) without rowid;
+
+-- Subscriptions Core
+
+create table if not exists subscription(
+  url text not null,
+  ts datetime default current_timestamp,
+  record_name text
+);
+
+create trigger if not exists subscription_bd before delete on subscription begin
+  delete from record where record_name=old.record_name;
+end;
+
+-- Subscriptions View
+
+create view if not exists subscription_view
+as select
+  r.record_name,
+  r.change_tag,
+  s.ts,
+  s.url
+from subscription s left join record r on s.record_name=r.record_name;
+
 -- Queue Core
 
 create table if not exists queued_entry(
@@ -22,15 +51,6 @@ create table if not exists queued_entry(
 ) without rowid;
 
 create unique index if not exists queued_entry_idx on queued_entry(record_name);
-
-create table if not exists record(
-  record_name text primary key,
-  change_tag text
-) without rowid;
-
-create trigger if not exists record_bd before delete on record begin
-  delete from queued_entry where record_name=old.record_name;
-end;
 
 create trigger if not exists queued_entry_bd before delete on queued_entry begin
   delete from record where record_name=old.record_name;
@@ -47,5 +67,12 @@ as select
   e.url,
   e.since
 from queued_entry e left join record r on e.record_name=r.record_name;
+
+-- Composite Triggers
+
+create trigger if not exists record_bd before delete on record begin
+  delete from queued_entry where record_name=old.record_name;
+  delete from subscription where record_name=old.record_name;
+end;
 
 commit transaction;
