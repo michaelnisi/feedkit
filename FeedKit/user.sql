f23536b3958318fb7e7db6aa25480e16c7002c38
Michael Nisi <michael.nisi@gmail.com>
2017-08-20 17:40:23 +0200

    Refine user database schema

--- FeedKit/user.sql
+++ FeedKit/user.sql
@@ -11,8 +11,6 @@ pragma user_version = 1;
 
 begin immediate;
 
--- Core
-
 -- CloudKit records
 
 create table if not exists record(
@@ -28,19 +26,7 @@ create table if not exists entry(
   url text not null
 ) without rowid;
 
--- Relations
-
-create table if not exists previous_entry(
-  guid text primary key,
-  ts datetime default current_timestamp,
-  record_name text
-) without rowid;
-
-create unique index if not exists previous_entry_idx on previous_entry(record_name);
-
-create trigger if not exists previous_entry_bd before delete on previous_entry begin
-  delete from record where record_name = old.record_name;
-end;
+-- Queued entries
 
 create table if not exists queued_entry(
   guid text primary key,
@@ -50,21 +36,35 @@ create table if not exists queued_entry(
 
 create unique index if not exists queued_entry_idx on queued_entry(record_name);
 
-create trigger if not exists queued_entry_bd before delete on queued_entry begin
-  delete from record where record_name = old.record_name;
+-- Previously queued entries
+
+create table if not exists previous_entry(
+  guid text primary key,
+  ts datetime default current_timestamp,
+  record_name text
+) without rowid;
+
+create unique index if not exists previous_entry_idx on previous_entry(record_name);
+
+-- Relations
+
+create trigger if not exists record_ad after delete on record begin
+  delete from queued_entry where record_name = old.record_name;
+  delete from previous_entry where record_name = old.record_name;
+end;
+
+create trigger if not exists queued_entry_ad after delete on queued_entry begin
   insert into previous_entry(guid) values(old.guid);
 end;
 
-create trigger if not exists queued_entry_bi before insert on queued_entry begin
+create trigger if not exists queued_entry_ad after insert on queued_entry begin
   delete from previous_entry where guid = new.guid;
 end;
 
-create trigger if not exists entry_bd before delete on entry begin
+create trigger if not exists entry_ad after delete on entry begin
   delete from queued_entry where guid = old.guid;
   delete from unqueued_entry where guid = old.guid;
 end;
-
--- TODO: Delete accumulated zombie entries
 
 -- All queued entries, including iCloud meta-data if synced
 
@@ -105,5 +105,9 @@ from entry e
 create view if not exists locally_previous_entry_view as
 select * from previous_entry_view
   where record_name is null;
+
+create view if not exists zombie_entry_guid_view as
+select guid from entry
+  where guid not in (select guid from queued_entry) and (select guid from previous_entry);
 
 commit;
