fe982edbcd230f1b08f9c5054d264dfde6a1b152
Michael Nisi <michael.nisi@gmail.com>
2018-05-02 18:36:51 +0200

    Describe feedkit error

--- FeedKit/core/FeedKitError.swift
+++ FeedKit/core/FeedKitError.swift
@@ -42,3 +42,60 @@ extension FeedKitError: Equatable {
     return lhs._code == rhs._code
   }
 }
+
+extension FeedKitError: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .unknown:
+      return "unkown error"
+    case .niy:
+      return "not implemented yet"
+    case .notAString:
+      return "not a string"
+    case .general(let message):
+      return "general error: \(message)"
+    case .cancelledByUser:
+      return "cancelled by user"
+    case .notAFeed:
+      return "not a feed"
+    case .notAnEntry:
+      return "not an entry"
+    case .serviceUnavailable(let er):
+      return "service unavailable: \(String(describing: er))"
+    case .feedNotCached(let urls):
+      return "feed not cached: \(urls)"
+    case .unknownEnclosureType(let type):
+      return "unknown enclosure type: \(type)"
+    case .multiple(let errors):
+      return "multiple errors: \(String(describing: errors))"
+    case .unexpectedJSON:
+      return "unexpected JSON"
+    case .sqlFormatting:
+      return "SQL formatting error"
+    case .cacheFailure(let er):
+      return "cache failure: \(String(describing: er))"
+    case .invalidSearchTerm(let term):
+      return "invalid search term: \(term)"
+    case .invalidEntry(let reason):
+      return "invalid entry: \(reason)"
+    case .invalidEntryLocator(let reason):
+      return "invalid entry locator: \(reason)"
+    case .invalidEnclosure(let reason):
+      return "invalid enclosure: \(reason)"
+    case .invalidFeed(let reason):
+      return "invalid feed: \(reason)"
+    case .invalidSuggestion(let reason):
+      return "invalid suggestion: \(reason)"
+    case .offline:
+      return "offline"
+    case .noForceApplied:
+      return "no force applied"
+    case .missingEntries(let locators):
+      return "missing entries: \(locators)"
+    case .unexpectedDatabaseRow:
+      return "unexpected database row"
+    case .unidentifiedFeed:
+      return "unidentified feed"
+    }
+  }
+}
